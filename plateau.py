bdfd10c3e6c9a594abf6138ad3fe38a3c271944d
AntoineGachet <antoine.gachet05@gmail.com>
2023-08-04 22:02:55 +0200

    board initialzed

--- plateau.py
+++ plateau.py
@@ -2,37 +2,23 @@ from piece import Piece
 
 class Plateau():
     def __init__(self):
-        self.cases = [
-            [Piece("Tour", "noir"), Piece("Cavalier", "noir"), Piece("Fou", "noir"), Piece("Reine", "noir"),
-             Piece("Roi", "noir"), Piece("Fou", "noir"), Piece("Cavalier", "noir"), Piece("Tour", "noir")],
-            [Piece("Pion", "noir") for _ in range(8)],
-            [" "]*8, [" "]*8, [" "]*8, [" "]*8,
-            [Piece("Pion", "blanc") for _ in range(8)],
-            [Piece("Tour", "blanc"), Piece("Cavalier", "blanc"), Piece("Fou", "blanc"), Piece("Reine", "blanc"),
-             Piece("Roi", "blanc"), Piece("Fou", "blanc"), Piece("Cavalier", "blanc"), Piece("Tour", "blanc")]
-        ]
-        self.joueur_actif = "blanc"
-        self.en_passant = None
-        self.roque = {"blanc": {"petit": True, "grand": True},
-                      "noir": {"petit": True, "grand": True}}
+        self.height = 8
+        self.width = 8
+        self.grid = None
     
-    def display_screen(self):
-        for i in range(8):
-            for j in range(8):
-                if self.cases[i][j] == " ":
-                    print(self.cases[i][j] + '  |', end='')
-                else:
-                    print("n" + "  |", end='')
-            print("\n", end="")
-            for k in range(8):
-                print("____", end="")
-            print("\n")
+    def set_grid(self):
+        grid = [['' for _ in range(self.width)] for _ in range(self.height)]
+        self.grid = grid
+        
+    
+    def display_grid(self):
+        for column in self.grid:
+            print(column)
+    
+    def __repr__(self):
+       return f'{self.grid}' 
 
-    def move_piece(self, piece, cord):
-        pass
+grid = Plateau()
 
-    def check_mate(self):
-        pass
-
-game = Plateau()
-game.display_screen()
+grid.set_grid()
+grid.display_grid()
