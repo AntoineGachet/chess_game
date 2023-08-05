82fa4bc8bd9e30cf421708a038eb30e62e6ff516
AntoineGachet <antoine.gachet05@gmail.com>
2023-08-05 13:38:15 +0200

    added the front of pieces as instance, team name changed

--- piece.py
+++ piece.py
@@ -2,10 +2,11 @@ from plateau import Plateau
 
 class Piece(Plateau):
 
-    def __init__(self, team, type, pos):
+    def __init__(self, team, front, pos):
         self.team = team
         self.pos = pos
         self.dir = None
+        self.front = front
     
     def rook(self):
         self.dir = {
@@ -14,12 +15,16 @@ class Piece(Plateau):
                 (0, v) for v in range(-8, 9)
         }
 
+        self.front = "♜" if self.team == "w" else "♖"
+
     def knight(self):
         self.dir = {
             (u, v) for u in range(-2, 2, 4) for v in range(-1, 1, 2)
             } | {
             (v, u) for u in range(-2, 2, 4) for v in range(-1, 1, 2)
             }
+        
+        self.front = "♞" if self.team == "w" else "♘"
 
     def bishop(self):
         self.dir = {
@@ -29,18 +34,27 @@ class Piece(Plateau):
             } | {
                 (u, self.width-u) for u in range(9)
             }
+        
+        self.front = "♝" if self.team == "w" else "♗"
+
 
     def king(self):
         self.dir = {
-            (u, v) for u in range(-1, 2)
+            (u, v = tuple(map(lambda i, j: i+j, self.pos, dir))) for u in range(-1, 2)
             for v in range(-1, 2)
             }
+        
+        self.front = "♚" if self.team == "w" else "♔"
 
     def queen(self):
         self.dir = self.bishop() | self.rook()
+
+        self.front = "♛" if self.team == "w" else "♕"
 
     def pawn(self):
         if self.team == 'white':
             self.dir = {(2,0), (1, 0)}
         else:
             self.dir = {(-2, 0), (-1, 0)} 
+        
+        self.front = "♟" if self.team == "w" else "♙"
