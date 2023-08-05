07760c485fcb375bb94ebc3d97aedc20e9bea064
AntoineGachet <antoine.gachet05@gmail.com>
2023-08-05 13:36:15 +0200

    pieces must move to valid coordinates (need completion for king)

--- mouvement.py
+++ mouvement.py
@@ -1,18 +1,38 @@
 from piece import Piece
 
 class Mouvements(Piece):
-    def __init__(self):
+    def __init__(self, move):
         self.available_move = []
+        self.move = move
+
+    def translate_mov(self):
+        x, y = self.move
+        try:
+            x =int(x)
+        except ValueError:
+            x = ord(x) -90
+            y = int(y)
+            return x, y
+        y = ord(y) - 90
+        return x, y
+
+    def outside_board(self, x, y):
+        if (x or y) >= 9 or (x or y) <= -9:
+            return True
 
     def can_move(self):
-        for dir in self.dir:
-            x, y = tuple(map(lambda i, j: i+j, self.pos, dir))
-            if self.grid[x][y] == '':
-                self.available_move.append((x, y))
-            
-            elif self.grid[x][y] in self.white_pieces:
-                if self.team == 'white':
-                    continue
+        # get the new positions
+        x, y = self.translate_move()
+        vector = tuple(map(lambda i, j: i - j, (x,y), self.move))
 
-                self.available_move.append((x, y))
-                continue
+        if self.outside_board(x, y):
+            return False
+
+        if not vector in self.dir:
+            return False
+
+        if self.grid[x][y] != "":
+            if self.grid[x][y].team == self.team:
+                return False
+
+        return True
