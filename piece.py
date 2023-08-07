d436930afe269dcc8409baa6a88fb34145093b43
AntoineGachet <antoine.gachet05@gmail.com>
2023-08-07 23:33:51 +0200

    methods now return the vectors

--- piece.py
+++ piece.py
@@ -1,60 +1,63 @@
-from plateau import Plateau
+class Piece:
 
-class Piece(Plateau):
-
-    def __init__(self, team, front, pos):
-        self.team = team
+    def __init__(self, piece_team, pos, front=None):
+        self.piece_team = piece_team
         self.pos = pos
-        self.dir = None
-        self.front = front
+        self.front = None
     
     def rook(self):
-        self.dir = {
+        dir = {
             (u, 0) for u in range(-8, 9)
             } | {
                 (0, v) for v in range(-8, 9)
         }
+        return dir
 
-        self.front = "♜" if self.team == "w" else "♖"
+        self.front = "♜" if self.piece_team == "w" else "♖"
 
     def knight(self):
-        self.dir = {
+        dir = {
             (u, v) for u in range(-2, 2, 4) for v in range(-1, 1, 2)
             } | {
             (v, u) for u in range(-2, 2, 4) for v in range(-1, 1, 2)
             }
         
-        self.front = "♞" if self.team == "w" else "♘"
+        self.front = "♞" if self.piece_team == "w" else "♘"
+        return dir
 
     def bishop(self):
-        self.dir = {
+        dir = {
             (u, u) for u in range(-8, 9)
             } | {
-                (u, self.width+u) for u in range(-8, 1) 
+                (u, 8+u) for u in range(-8, 1) 
             } | {
-                (u, self.width-u) for u in range(9)
+                (u, 8-u) for u in range(9)
             }
+        return dir
+    
+        self.front = "♝" if self.piece_team == "w" else "♗"
         
-        self.front = "♝" if self.team == "w" else "♗"
-
 
     def king(self):
-        self.dir = {
-            (u, v = tuple(map(lambda i, j: i+j, self.pos, dir))) for u in range(-1, 2)
+        dir = {
+            (u, v) for u in range(-1, 2)
             for v in range(-1, 2)
             }
         
-        self.front = "♚" if self.team == "w" else "♔"
-
+        self.front = "♚" if self.piece_team == "w" else "♔"
+        return dir
+    
     def queen(self):
-        self.dir = self.bishop() | self.rook()
+        dir = self.bishop() | self.rook()
 
-        self.front = "♛" if self.team == "w" else "♕"
-
+        self.front = "♛" if self.piece_team == "w" else "♕"
+        return dir
+    
     def pawn(self):
-        if self.team == 'white':
-            self.dir = {(2,0), (1, 0)}
+        if self.piece_team == 'white':
+            dir = {(2,0), (1, 0)}
         else:
-            self.dir = {(-2, 0), (-1, 0)} 
+            dir = {(-2, 0), (-1, 0)} 
         
-        self.front = "♟" if self.team == "w" else "♙"
+        self.front = "♟" if self.piece_team == "w" else "♙"
+        return dir  
