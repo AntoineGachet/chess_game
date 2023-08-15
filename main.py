f95f6ab2fc4c9b2e3b62b163420134fa1e10ab0b
AntoineGachet <antoine.gachet05@gmail.com>
2023-08-15 22:01:02 +0200

    the program reruns on main, with alternating turn working

--- main.py
+++ main.py
@@ -1,78 +1,33 @@
 from plateau import Plateau
-from piece import Piece
-from mouvement import Mouvements
 from joueurs import Joueurs
 
-check_mated = False
+joueur_1 = Joueurs('w')
+joueur_2 = Joueurs('b')
 
-black = [
-    Piece('b').rook(),
-    Piece('b').knight(),
-    Piece('b').bishop(),
-    Piece('b').king(),
-    Piece('b').queen(),
-    Piece('b').bishop(),
-    Piece('b').knight(),
-    Piece('b').rook(),
-    Piece('b').pawn(),
-    Piece('b').pawn(),
-    Piece('b').pawn(),
-    Piece('b').pawn(),
-    Piece('b').pawn(),
-    Piece('b').pawn(),
-    Piece('b').pawn(),
-    Piece('b').pawn(),
-]
+def play(board, joueur):
+    board.display_grid()
+    moved = False
+    board.reset_en_passant(joueur)
 
-white = [
-    Piece('w').rook(),
-    Piece('w').knight(),
-    Piece('w').bishop(),
-    Piece('w').king(),
-    Piece('w').queen(),
-    Piece('w').bishop(),
-    Piece('w').knight(),
-    Piece('w').rook(),
-    Piece('w').pawn(),
-    Piece('w').pawn(),
-    Piece('w').pawn(),
-    Piece('w').pawn(),
-    Piece('w').pawn(),
-    Piece('w').pawn(),
-    Piece('w').pawn(),
-    Piece('w').pawn(),
-]
+    while not moved:
+        move_from, move_to = joueur.ask_move()
+        from_x, from_y = board.translate_move(move_from)
+        to_x, to_y = board.translate_move(move_to)
+    
+        piece = board.grid[from_y][from_x]
+        vector = piece.get_dir(from_x, from_y, to_x, to_y)
+        if board.move(joueur.team, from_x, from_y, to_x, to_y, vector):
+            moved = True
+        
+            
 
 board = Plateau()
-board.set_grid(white, black)
-player_1 = Joueurs('w')
-player_2 = Joueurs('b')
-
 def main():
-    turn = 0
-    while not check_mated:
-        if turn%2 == 0:
-            board.display_grid()
-            move_1 = player_1.ask_move()
-            mouvements = Mouvements(move_1, turn, board)
-            x_1, y_1 = mouvements.translate_mov((move_1[0]))
-            x_2, y_2 =mouvements.translate_mov(move_1[1])
-            mouvements.can_move(x_1, y_1, x_2, y_2)
-            board.update_grid(move_1[0], move_1[1])
-            board.display_grid()
-
-            turn +=1
-
-        else:
-            move_2 = player_2.ask_move()
-            mouvements = Mouvements(move_2, turn)
-            x_1, y_1 = mouvements.translate_mov(move_2[0])
-            x_2, y_2 =mouvements.translate_mov(move_2[1])
-            mouvements.can_move(x_1, y_1, x_2, y_2)
-            board.update_grid(move_2[0], move_2[1])
-            board.display_grid()
-
-            turn +=1
+    n = 0
+    while True:
+        joueur = joueur_1 if n%2 == 0 else joueur_2
+        n+=1
+        play(board, joueur)
 
 if __name__ == '__main__':
     main()
